5cd51225657b3f8e6eb5a474ccd838af54248f0d
João Vitor da Silva <joaomk@live.com>
2022-09-17 04:15:43 -0300

    Fourth class - Mobile

--- mobile/src/components/GameCard/index.tsx
+++ mobile/src/components/GameCard/index.tsx
@@ -1,13 +1,15 @@
-import { Text, TouchableOpacity, TouchableOpacityProps, ImageBackground, ImageSourcePropType } from 'react-native';
+import { Text, TouchableOpacity, TouchableOpacityProps, ImageBackground } from 'react-native';
 import { LinearGradient } from 'expo-linear-gradient';
 import { styles } from './styles';
 import { THEME } from '../../theme';
 
 export interface GameCardProps {
   id: string;
-  name: string;
-  ads: string;
-  cover: ImageSourcePropType;
+  title: string;
+  _count: {
+    ads: number
+  };
+  bannerUrl: string;
 }
 
 interface Props extends TouchableOpacityProps{
@@ -19,7 +21,7 @@ export function GameCard({ data, ...rest }: Props) {
     <TouchableOpacity style={styles.container} {...rest}>
       <ImageBackground 
         style={styles.cover}
-        source={data.cover}
+        source={{uri: data.bannerUrl}}
       >
 
       <LinearGradient
@@ -27,10 +29,10 @@ export function GameCard({ data, ...rest }: Props) {
         style={styles.footer}
       >
         <Text style={styles.name}>
-          {data.name}
+          {data.title}
         </Text>
         <Text style={styles.ads}>
-          {data.ads} anúncios
+          {data._count.ads} anúncios
         </Text>
       </LinearGradient>
     </ImageBackground>
