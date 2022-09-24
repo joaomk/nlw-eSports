0fbad405f268694040b3c047deb2ec172da59aee
João Vitor da Silva <joaomk@live.com>
2022-09-24 01:58:44 -0300

    Fifth class - Mobile

--- mobile/src/screens/Game/styles.ts
+++ mobile/src/screens/Game/styles.ts
@@ -1,10 +1,10 @@
-import { StyleSheet } from 'react-native';
-import { THEME } from '../../theme';
+import { StyleSheet } from "react-native";
+import { THEME } from "../../theme";
 
 export const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
+    alignItems: "center",
   },
   header: {
     width: '100%',
@@ -12,21 +12,21 @@ export const styles = StyleSheet.create({
     alignItems: 'center',
     paddingHorizontal: 32,
     marginTop: 28,
-    justifyContent: 'center',
+    justifyContent: 'space-between',
   },
   logo: {
     width: 72,
-    height: 40
+    height: 40,
   },
   right: {
     width: 20,
-    height: 20,
+    height: 20
   },
   cover: {
     width: 311,
     height: 160,
     borderRadius: 8,
-    marginTop: 32
+    marginTop: 32,
   },
   containerList: {
     width: '100%',
@@ -34,16 +34,16 @@ export const styles = StyleSheet.create({
   contentList: {
     paddingLeft: 32,
     paddingRight: 64,
-    alignItems: 'flex-start',
+    alignItems: 'flex-start'
   },
   emptyListText: {
     color: THEME.COLORS.CAPTION_300,
     fontSize: THEME.FONT_SIZE.SM,
-    fontFamily: THEME.FONT_FAMILY.REGULAR
+    fontFamily: THEME.FONT_FAMILY.REGULAR,
   },
   emptyListContent: {
-    flex: 1, 
-    alignItems: 'center',
+    flex: 1,
     justifyContent: 'center',
+    alignItems: 'center',
   }
-});
+})
